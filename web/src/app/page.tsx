b2155b9b6964d40e9373acf8fbb5c621df84e754
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-03-02 18:25:07 -0800

    home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,17 @@
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  return <HydrateClient>a</HydrateClient>;
+  return (
+    <HydrateClient>
+      <div className="flex h-dvh w-dvw flex-col items-center justify-center gap-2">
+        <div className="flex items-center gap-2 text-3xl">
+          <span>AI Tool</span>
+          <span className="rounded-md bg-orange-500 px-1 py-0.5 text-black">
+            Hub
+          </span>
+        </div>
+        <span className="text-primary/30">Coming soon</span>
+      </div>
+    </HydrateClient>
+  );
 }
