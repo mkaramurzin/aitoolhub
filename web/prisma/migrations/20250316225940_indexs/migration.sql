e993dcb07e6f7bbb197f8115f0e6b68d51a75765
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-03-16 15:59:43 -0700

    index

---
+++ web/prisma/migrations/20250316225940_indexs/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Tool_pricing_idx" ON "Tool"("pricing");
+
+-- CreateIndex
+CREATE INDEX "Tool_description_idx" ON "Tool"("description");
+
+-- CreateIndex
+CREATE INDEX "Tool_rating_idx" ON "Tool"("rating");
