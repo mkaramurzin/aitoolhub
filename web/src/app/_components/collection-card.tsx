d78938bf37e85ff823e190c227d4713786bc8bd2
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-03-31 20:36:38 -0700

    Fix

--- web/src/app/_components/collection-card.tsx
+++ web/src/app/_components/collection-card.tsx
@@ -20,8 +20,8 @@ function CollectionCard({ collection, tools }: CollectionCardProps) {
       />
 
       {/* Background gradient */}
-      <div className="to-slate-[#000000] absolute bottom-0 left-0 h-2/6 w-full bg-gradient-to-t from-slate-800/70 transition-all duration-500 group-hover:h-3/6"></div>
-      <div className="to-slate-[#000000] absolute left-0 top-0 h-2/6 w-full bg-gradient-to-b from-slate-800/70 transition-all duration-500 group-hover:h-3/6"></div>
+      <div className="to-slate-[#000000] absolute bottom-0 left-0 h-2/6 w-full rounded-b-md bg-gradient-to-t from-slate-800/70 transition-all duration-500 group-hover:h-3/6"></div>
+      <div className="to-slate-[#000000] absolute left-0 top-0 h-2/6 w-full rounded-t-md bg-gradient-to-b from-slate-800/70 transition-all duration-500 group-hover:h-3/6"></div>
 
       <div className="z-10 flex w-full text-start">
         <div className="flex w-full flex-col text-2xl font-semibold drop-shadow-lg">
@@ -31,7 +31,7 @@ function CollectionCard({ collection, tools }: CollectionCardProps) {
 
       <div className="z-10 flex flex-col text-start">
         <div className="flex w-full flex-col justify-between gap-4 md:flex-row md:items-end">
-          <div className="flex gap-2">
+          <div className="flex flex-wrap items-end gap-1 space-x-reverse md:max-w-[50%]">
             {tools.map((tool) => (
               <img
                 key={tool.id}
